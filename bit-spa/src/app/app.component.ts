6de3c266a04589f2044f1a3f2be5de0a41ed263e
Alexa921 <alexandrarestrepoagudelo451@gmail.com>
2025-02-12 21:57:25 -0500

    Seguimiento del proyecto

--- bit-spa/src/app/app.component.ts
+++ bit-spa/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from "./components/home/home.component";
+import { FoundationsComponent } from './components/foundations/foundations.component';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, NavComponent, HomeComponent, FooterComponent, FoundationsComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- bit-spa/src/app/app.routes.ts
+++ bit-spa/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component"
+import { FoundationsComponent } from './components/foundations/foundations.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'contact', component: ContactComponent},
+    {path: "inicio", component: HomeComponent},
+    {path: "fundamentos", component: FoundationsComponent},
+];
