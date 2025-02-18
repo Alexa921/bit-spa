d82685c7c6867006b4755305dea86c9952655414
Alexa921 <alexandrarestrepoagudelo451@gmail.com>
2025-02-17 21:30:50 -0500

    Ultimas actualizaciones

--- bit-spa/src/app/app.routes.ts
+++ bit-spa/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component"
 import { FoundationsComponent } from './components/foundations/foundations.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: 'contact', component: ContactComponent },
-    { path: "inicio", component: HomeComponent },
-    { path: "fundamentos", component: FoundationsComponent },
+    { path: 'contact', component: ContactComponent, title: "Contacto" },
+    { path: "fundamentos", component: FoundationsComponent, title: "Populares" },
+    { path: "inicio", component: HomeComponent, title: "Inicio" },
+    { path: "", redirectTo: "inicio", pathMatch: "full" },
+    { path: "**", component: PageNotFoundComponent, title: "Error 404" },
 ];
